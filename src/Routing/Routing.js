2bdeb20f946db62b968b5b7a01c7e25427089629
Ashikur Rahman <smashikurrahman24@gmail.com>
2022-07-19 16:42:13 +0600

    navbar updated

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../components/Home/Home/Home';
 import MealDetails from '../components/Home/Meal/MealDetails';
+import Header from '../components/Shared/Header/Header';
 
 const Routing = () => {
     return (
         <BrowserRouter>
+        <Header></Header>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/mealdetails/:mealId" element={<MealDetails />} />
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import icon from '../../../images/logo2.png';
+import Button from "react-bootstrap/Button";
 
 const Header = () => {
     return (
-        <div>
-            This is header
-        </div>
+        <>
+            <Navbar
+                sticky="top"
+                collapseOnSelect
+                expand="lg"
+                bg="primaryyuio  "
+                variant="dark"
+            >
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={icon}
+                            width="120"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav className="">
+                            <Nav.Link as={Link} to="/home">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/about">
+                                About
+                            </Nav.Link>
+
+                            {/* {!users.email ? (
+                                <Nav.Link as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                            ) : (
+                                <button onClick={logOut}>SignOut</button>
+                            )} */}
+                        </Nav>
+                        {/* {users.email ? (
+                            <Navbar.Text>
+                                Signed in as:{" "}
+                                <a href="#login">{users.displayName}</a>
+                            </Navbar.Text>
+                        ) : (
+                            ""
+                        )} */}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
